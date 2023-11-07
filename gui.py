bdbb3cea9f11ebe69247c6a2862ea399bb50b45b
Igor Dyachenko <fanfanya2018@gmail.com>
2023-11-07 05:10:00 +0100

    Created a basic GUI without interactions, day 16

---
+++ gui.py
@@ -0,0 +1,11 @@
+import functions
+import time
+import PySimpleGUI as Sg
+
+label = Sg.Text("Type in a to-do")
+input_box = Sg.InputText(tooltip="Enter a todo")
+add_button = Sg.Button("Add")
+
+window = Sg.Window("My To-Do App", layout=[[label, input_box, add_button]])
+window.read()
+window.close()
