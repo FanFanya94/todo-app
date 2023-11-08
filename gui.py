c429f374906ade06b4d9a3a33b5673b400b1c631
Igor Dyachenko <fanfanya2018@gmail.com>
2023-11-08 05:26:58 +0100

    Implement an Add button functionality, day 17

--- gui.py
+++ gui.py
@@ -2,10 +2,29 @@ import functions
 import time
 import PySimpleGUI as Sg
 
+# Window elements definition
 label = Sg.Text("Type in a to-do")
-input_box = Sg.InputText(tooltip="Enter a todo")
+input_box = Sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = Sg.Button("Add")
 
-window = Sg.Window("My To-Do App", layout=[[label, input_box, add_button]])
-window.read()
+# Window definition
+window = Sg.Window("My To-Do App",
+                   layout=[[label, input_box, add_button]],
+                   font=("Helvetica", 20))
+
+# Window run
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+
+    match event:
+        case "Add":
+            todos = functions.get_list_from_textfile()
+            new_todo = values["todo"]
+            todos.append(new_todo)
+            functions.save_list_to_file(todos)
+        case Sg.WIN_CLOSED:
+            break
+
 window.close()
