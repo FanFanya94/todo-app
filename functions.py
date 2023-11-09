98cae853b5f8a056e5a1c5f508946e77dd9a9810
Igor Dyachenko <fanfanya2018@gmail.com>
2023-11-09 04:25:07 +0100

    Implement a Save button  functionality and list indexes, day 17

--- functions.py
+++ functions.py
@@ -20,3 +20,16 @@ def get_list_from_textfile(filename=DEFAULT_FILEPATH):
     loaded_list = [item_.strip() for item_ in loaded_list]
 
     return loaded_list
+
+
+def indexate_list(list_to_indexate):
+    """
+    Gets a list and returns the new list of tuples with indexes beginning from 1
+    and values of original list. Ex: [(1, value1), (2, value2), ... (N, valueN)]
+    """
+    indexed_list = []
+
+    for index, value in enumerate(list_to_indexate):
+        indexed_list.append((str(index + 1) + '.', value))
+
+    return indexed_list
--- gui.py
+++ gui.py
@@ -2,18 +2,23 @@ import functions
 import time
 import PySimpleGUI as Sg
 
+# Get todos list from file
+todos = functions.get_list_from_textfile()
+
 # Window elements definition
 label = Sg.Text("Type in a to-do")
 input_box = Sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = Sg.Button("Add")
 edit_button = Sg.Button("Edit")
-list_box = Sg.Listbox(values=functions.get_list_from_textfile(), key="list",
+save_button = Sg.Button("Save")
+list_box = Sg.Listbox(values=functions.indexate_list(todos), key="list",
                       enable_events=True, size=(45, 10))
 
 # Window definition
 window = Sg.Window("My To-Do App",
                    layout=[[label, input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button],
+                           [save_button]],
                    font=("Helvetica", 20))
 
 # Window run
@@ -24,22 +29,18 @@ while True:
 
     match event:
         case "Add":
-            todos = functions.get_list_from_textfile()
             new_todo = values["todo"]
             todos.append(new_todo)
-            functions.save_list_to_file(todos)
-            window["list"].update(values=todos)
+            window["list"].update(values=functions.indexate_list(todos))
         case "Edit":
-            todo_to_edit = values["list"][0]
+            index_of_todo_to_edit = int(values["list"][0][0].strip('.'))
             new_todo_instead = values["todo"]
-
-            todos = functions.get_list_from_textfile()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo_instead
-            functions.save_list_to_file(todos)
-            window["list"].update(values=todos)
+            todos[index_of_todo_to_edit - 1] = new_todo_instead
+            window["list"].update(values=functions.indexate_list(todos))
         case "list":
-            window["todo"].update(value=values["list"][0])
+            window["todo"].update(value=values["list"][0][1])
+        case "Save":
+            functions.save_list_to_file(todos)
         case Sg.WIN_CLOSED:
             break
 
