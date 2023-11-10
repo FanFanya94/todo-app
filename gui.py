2329915f1c11ad198706be0ea04ed2e3b239e37c
Igor Dyachenko <fanfanya2018@gmail.com>
2023-11-10 06:00:45 +0100

    Implement a Complete and Exit buttons'  functionality, day 18

--- gui.py
+++ gui.py
@@ -11,14 +11,16 @@ input_box = Sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = Sg.Button("Add")
 edit_button = Sg.Button("Edit")
 save_button = Sg.Button("Save")
+complete_button = Sg.Button("Complete")
+exit_button = Sg.Button("Exit")
 list_box = Sg.Listbox(values=functions.indexate_list(todos), key="list",
-                      enable_events=True, size=(45, 10))
+                      enable_events=True, size=(45, 25))
 
 # Window definition
 window = Sg.Window("My To-Do App",
                    layout=[[label, input_box, add_button],
-                           [list_box, edit_button],
-                           [save_button]],
+                           [list_box, edit_button, complete_button],
+                           [save_button, exit_button]],
                    font=("Helvetica", 20))
 
 # Window run
@@ -27,21 +29,32 @@ while True:
     print(event)
     print(values)
 
-    match event:
-        case "Add":
-            new_todo = values["todo"]
-            todos.append(new_todo)
-            window["list"].update(values=functions.indexate_list(todos))
-        case "Edit":
-            index_of_todo_to_edit = int(values["list"][0][0].strip('.'))
-            new_todo_instead = values["todo"]
-            todos[index_of_todo_to_edit - 1] = new_todo_instead
-            window["list"].update(values=functions.indexate_list(todos))
-        case "list":
-            window["todo"].update(value=values["list"][0][1])
-        case "Save":
-            functions.save_list_to_file(todos)
-        case Sg.WIN_CLOSED:
-            break
+    try:
+        match event:
+            case "Add":
+                new_todo = values["todo"]
+                todos.append(new_todo)
+                window["list"].update(values=functions.indexate_list(todos))
+                window["todo"].update(value="")
+            case "Edit":
+                index_of_todo_to_edit = int(values["list"][0][0].strip('.'))
+                new_todo_instead = values["todo"]
+                todos[index_of_todo_to_edit - 1] = new_todo_instead
+                window["list"].update(values=functions.indexate_list(todos))
+            case "list":
+                window["todo"].update(value=values["list"][0][1])
+            case "Complete":
+                index_of_todo_to_complete = int(values["list"][0][0].strip('.')) - 1
+                del todos[index_of_todo_to_complete]
+                window["list"].update(values=functions.indexate_list(todos))
+            case "Save":
+                functions.save_list_to_file(todos)
+            case "Exit":
+                break
+            case Sg.WIN_CLOSED:
+                break
+
+    except IndexError:
+        continue
 
 window.close()
